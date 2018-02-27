46e07a889f3b691b50c6a032aca75bef9c6c8ff5
n2omatt <n2omatt@amazingcow.com>
2018-02-27 01:21:42 -0300

    - Add report facilities.

--- src/report.sh
+++ src/report.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env sh
 ##~---------------------------------------------------------------------------##
 ##                        ____                       _   _                    ##
 ##                  _ __ |___ \ ___  _ __ ___   __ _| |_| |_                  ##
@@ -6,9 +5,9 @@
 ##                 | | | |/ __/ (_) | | | | | | (_| | |_| |_                  ##
 ##                 |_| |_|_____\___/|_| |_| |_|\__,_|\__|\__|                 ##
 ##                              www.n2omatt.com                               ##
-##  File      : gitcheck_update_remotes.sh                                    ##
-##  Project   : TSP                                                           ##
-##  Date      : Feb 24, 2018                                                  ##
+##  File      : report.sh                                                     ##
+##  Project   :                                                               ##
+##  Date      : Feb 27, 2018                                                  ##
 ##  License   : GPLv3                                                         ##
 ##  Author    : n2omatt <n2omatt@amazingcow.com>                              ##
 ##  Copyright : n2omatt - 2018                                                ##
@@ -21,20 +20,27 @@
 ## Imports                                                                    ##
 ##----------------------------------------------------------------------------##
 source /usr/local/src/acow_shellscript_utils.sh
-source ./report.sh
 
 
 ##----------------------------------------------------------------------------##
-## Vars                                                                       ##
+## Functions                                                                  ##
 ##----------------------------------------------------------------------------##
-PROGRAM_NAME="gitcheck_update_remotes"
+get_report_file_path()
+{
+    local PROGRAM_NAME="$1";
 
-USER_HOME=$(get_user_home n2omatt);
-REPORT_FILE=$(get_report_file_path "$PROGRAM_NAME");
+    local CURR_DATE=$(date +"%Y_%m_%d_%H_%M_%S");
+    local USER_HOME="$(get_user_home n2omatt)";
+    local REPORT_DIR="$USER_HOME/.cron_scripts/$PROGRAM_NAME";
+    local REPORT_FILE="$REPORT_DIR/$CURR_DATE";
 
+    ##--------------------------------------------------------------------------
+    ## Create the report directory - We want it tidy.
+    mkdir -p "$REPORT_DIR";
 
-##----------------------------------------------------------------------------##
-## Script                                                                     ##
-##----------------------------------------------------------------------------##
-cd "${USER_HOME}/Documents/Projects";
-gitcheck --remote --quiet > "$REPORT_FILE";
+    ##--------------------------------------------------------------------------
+    ## Create the report file.
+    touch "$REPORT_FILE";
+
+    echo "$REPORT_FILE";
+}
